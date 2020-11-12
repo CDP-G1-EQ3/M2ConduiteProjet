9e0008352fd7aa5eed93ba8488d21af128d03585
Adri <adrien.boitelle@gmail.com>
2020-11-12 21:41:09 +0100

    Close T17

---
+++ code/db/sql/06_sprint.sql
@@ -0,0 +1,14 @@
+CREATE TABLE cdp_sprint (
+  project int UNSIGNED NOT NULL,
+  id int UNSIGNED AUTO_INCREMENT,
+
+  INDEX I_PROJECT (project),
+  INDEX I_ID (ID),
+  CONSTRAINT PK_ID PRIMARY KEY (project, id)
+);
+
+
+INSERT INTO `cdp_sprint`(`project`) 
+        VALUES (1);
+
+UPDATE `cdp_us` SET sprint = 1 WHERE project = 1 AND id = 1;
