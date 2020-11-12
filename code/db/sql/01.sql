94709bb898b3554417c44ea37a4e209bf4bb8cf6
Adri <adrien.boitelle@gmail.com>
2020-11-12 19:33:29 +0100

    add test data on db build

--- code/db/sql/01.sql
+++ code/db/sql/01.sql
@@ -12,3 +12,6 @@ CREATE TABLE cdp_user (
 
     CONSTRAINT PK_ID PRIMARY KEY (id)
 );
+
+INSERT INTO `cdp_user`(`identifier`, `username`, `mail`, `sha`) 
+        VALUES ("adri", "Adri", "adrien.boitelle@gmail.com", "");
