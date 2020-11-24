e12e502da3def0b9a2367ac1657edc68eb0ec13c
elhadja <elhadja007@gmail.com>
2020-11-24 12:15:43 +0100

    implementation de l'enregistrement et de la connexionn d'un utilisateur (backend)

--- code/web/server/controllers/RootController.js
+++ code/web/server/controllers/RootController.js
@@ -1,4 +1,5 @@
 const Controller = require("../Controller");
+const bcrypt = require("bcrypt");
 
 class RootController extends Controller {
 
@@ -8,6 +9,27 @@ class RootController extends Controller {
 
     get(req, res) {
         res.render("login");
+    }
+
+    post(req, res) {
+        Controller.dm.getUserFromIdentifier(req.body.username)
+            .then(sqlResult => {
+                if (sqlResult.length !== 0) {
+                    bcrypt.compare(req.body.password, sqlResult[0].sha)
+                        .then(valid => {
+                            if (!valid)
+                                res.json({ message: "invalid passord" });
+                            else {
+                                res.json({ message: "user logged " });
+                            }
+                        })
+                }
+                else
+                    res.json({ message: "no user with this username" })
+            })
+            .catch(err => {
+                res.json({error: err});
+            });
     }
 }
 
