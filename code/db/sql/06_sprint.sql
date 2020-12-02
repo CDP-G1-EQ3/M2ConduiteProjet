68f3067b83751b2e34424b4a8b9c4e8ec3a49232
elhadja <elhadja007@gmail.com>
2020-12-02 11:38:58 +0100

    modification des tables sprint et task

--- code/db/sql/06_sprint.sql
+++ code/db/sql/06_sprint.sql
@@ -1,6 +1,11 @@
 CREATE TABLE cdp_sprint (
-  project int UNSIGNED NOT NULL,
   id int UNSIGNED AUTO_INCREMENT,
+  project int UNSIGNED NOT NULL,
+  name TEXT NOT NULL,
+  state ENUM('finished', 'active'),
+  startDate DATE,
+  endDate DATE,
+
 
   INDEX I_PROJECT (project),
   INDEX I_ID (id),
--- code/db/sql/zz_initializeWithData.sql
+++ code/db/sql/zz_initializeWithData.sql
@@ -29,13 +29,26 @@ INSERT INTO `cdp_user_project`(`user`, `project`, `role`)
         VALUES (2,4,"owner");
 
 
+INSERT INTO `cdp_us`(`project`, `label`, `difficulty`, `sprint`) 
+        VALUES (1,"En tant que a, je souhaite y afin de z",5, 1);
+
+INSERT INTO `cdp_us`(`project`, `label`, `difficulty`, `sprint`) 
+        VALUES (1,"En tant que b, je souhaite y afin de z",6, 1);
+
+INSERT INTO `cdp_us`(`project`, `label`, `difficulty`, `sprint`) 
+        VALUES (1,"En tant que b, je souhaite y afin de z",6, 2);
+
+INSERT INTO `cdp_us`(`project`, `label`, `difficulty`, `sprint`) 
+        VALUES (1,"En tant que b, je souhaite y afin de z",6, 2);
+
 INSERT INTO `cdp_us`(`project`, `label`, `difficulty`) 
-        VALUES (1,"En tant que a, je souhaite y afin de z",5);
-INSERT INTO `cdp_us`(`project`, `label`, `difficulty`) 
-        VALUES (1,"En tant que b, je souhaite y afin de z",6);
+        VALUES (1,"En tant que c, je souhaite y afin de z",17);
         
 INSERT INTO `cdp_us`(`project`, `label`, `difficulty`) 
-        VALUES (1,"En tant que c, je souhaite y afin de z",7);
+        VALUES (1,"En tant que c, je souhaite y afin de z",27);
+
+
+
 
 
 INSERT INTO `cdp_us`(`project`, `label`, `difficulty`) 
@@ -46,3 +59,26 @@ INSERT INTO `cdp_us`(`project`, `label`, `difficulty`)
         
 INSERT INTO `cdp_us`(`project`, `label`, `difficulty`) 
         VALUES (2,"En tant que c, je souhaite y afin de z",7);
+
+
+/* ajout de tâche pour le premier projet */
+INSERT INTO `cdp_task`(`project`, `title`) VALUES (1, "description la tâche 1");
+INSERT INTO `cdp_task`(`project`, `title`) VALUES (1, "description la tâche 2");
+INSERT INTO `cdp_task`(`project`, `title`) VALUES (1, "description la tâche 3");
+INSERT INTO `cdp_task`(`project`, `title`) VALUES (1, "description la tâche 4");
+INSERT INTO `cdp_task`(`project`, `title`) VALUES (1, "description la tâche 5");
+/* taches faisant réference à d'autres US */
+INSERT INTO `cdp_task`(`project`, `title`, `us`) VALUES (1, "une description 6", 1);
+INSERT INTO `cdp_task`(`project`, `title`, `us`) VALUES (1, "une description 7", 1);
+INSERT INTO `cdp_task`(`project`, `title`, `us`) VALUES (1, "une description 8", 2);
+
+/* ajout de dependances pour quelques tâches */
+INSERT INTO `cdp_task_dep`(`project`, `task`, `dep`) VALUES (1, 1, 2);
+INSERT INTO `cdp_task_dep`(`project`, `task`, `dep`) VALUES (1, 1, 3);
+INSERT INTO `cdp_task_dep`(`project`, `task`, `dep`) VALUES (1, 2, 4);
+INSERT INTO `cdp_task_dep`(`project`, `task`, `dep`) VALUES (1, 2, 5);
+
+/* création de deux deux sprints pour le prémier projet */
+INSERT INTO `cdp_sprint` (`project`, `name`, `state`) VALUES (1, "sprint 1", "active");
+INSERT INTO `cdp_sprint` (`project`, `name`) VALUES (1, "sprint 2");
+INSERT INTO `cdp_sprint` (`project`, `name`) VALUES (1, "sprint 3");
