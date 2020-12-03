14746157330aa3886be7a74baf1d6dd934c6278c
elhadja <elhadja007@gmail.com>
2020-12-03 19:28:03 +0100

    implementation de drag and drop sur les items dans backlog.ejs

--- code/web/data/js/backlog.js
+++ code/web/data/js/backlog.js
@@ -41,3 +41,41 @@ for (let i=0; i<backlogLines.length; i++) {
 function onFormSubmit(form) {
     form.action = "/backlog/us/" + usId;
 }
+
+
+//------------------------ drag and drop
+let listItems = document.querySelectorAll(".draggable");
+
+for (let i=0; i<listItems.length; i++) {
+    listItems[i].addEventListener("dragstart", (ev) => {
+        ev.dataTransfer.setData("text", ev.target.id)
+        console.debug("drag start");
+    });
+}
+
+
+let sourceAndDestinationElements = document.querySelectorAll(".sourceAndDestination");
+console.log(sourceAndDestinationElements)
+
+sourceAndDestinationElements.forEach(element => {
+    console.log("sourceDest")
+    element.addEventListener("drop", (ev) => {
+        ev.preventDefault();
+        let data = ev.dataTransfer.getData("text");
+        let dropzone = ev.target;
+        while (!dropzone.className.includes("sourceAndDestination")) {
+            dropzone = dropzone.parentNode;
+        }
+        dropzone.appendChild(document.getElementById(data));
+        console.log("target", dropzone);
+        console.log("droped");
+    })
+
+    element.addEventListener("dragover", (ev) => {
+        ev.preventDefault();
+        console.log("dragover");
+})
+});
+
+
+
